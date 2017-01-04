4cbf48e36ffa080611182164cf84b12cc25e6888
Juan Jose Lledo Frasquet <juan.lledofrasquet@student.manchester.ac.uk>
2017-01-04 02:37:07 +0100

    Add functionality for users other than me

--- HSDownload.py
+++ HSDownload.py
@@ -1,10 +1,12 @@
+from os.path import expanduser
 import pyautogui
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-chromedriver_path = "/Users/Juan/Desktop/chromedriver"
+home = expanduser("~")
+chromedriver_path = home + "/Desktop/chromedriver"
 chrome_options = webdriver.ChromeOptions()
 local_state = {"protocol_handler.excluded_schemes.magnet" : False}
 chrome_options.add_experimental_option("localState",local_state)
@@ -17,7 +19,7 @@ driver.get(url)
 WebDriverWait(driver, 5).until(EC.title_contains("HorribleSubs"))
 
 links = driver.find_elements_by_xpath("//a[text()='1080p']")
-print("Number of 1080p buttons found: " + str(len(links)))
+#print("Number of 1080p buttons found: " + str(len(links)))
 
 magnets = driver.find_elements_by_xpath("//div[contains(@class, '1080p')] \
                                         //a[@title='Magnet Link']")
